71c26b9ebb9585e2e72780872bf51f66c03d83eb
MCabal77 <marccabal77@gmail.com>
2022-11-03 22:01:40 -0700

    Change the seekbar colors to an accessible color

--- src/components/MusicPlayer/VolumeBar.jsx
+++ src/components/MusicPlayer/VolumeBar.jsx
@@ -13,7 +13,7 @@ const VolumeBar = ({ value, min, max, onChange, setVolume }) => (
       min={min}
       max={max}
       onChange={onChange}
-      className="2xl:w-40 lg:w-32 md:w-32 h-1 ml-2 accent-[#A020F0]"
+      className="2xl:w-40 lg:w-32 md:w-32 h-1 ml-2 accent-[#BA55D3]"
     />
   </div>
 );
