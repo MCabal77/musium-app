2e779048b042bd59c11a86870783a4e8a59d6286
MCabal77 <marccabal77@gmail.com>
2022-11-03 21:39:38 -0700

    Change the seekers for track and volume

--- src/components/MusicPlayer/Controls.jsx
+++ src/components/MusicPlayer/Controls.jsx
@@ -6,12 +6,14 @@ const Controls = ({ isPlaying, repeat, setRepeat, shuffle, setShuffle, currentSo
   <div className="flex items-center justify-around md:w-36 lg:w-52 2xl:w-80">
     {/* Repeat */}
     <BsArrowRepeat size={20} color={repeat ? '#301934' : 'white'} onClick={() => setRepeat((prev) => !prev)} className="hidden sm:block cursor-pointer" />
+    {/* Previous */}
     {currentSongs?.length && <MdSkipPrevious size={30} color="#FFF" className="cursor-pointer" onClick={handlePrevSong} />}
     {isPlaying ? (
       <BsFillPauseFill size={45} color="#FFF" onClick={handlePlayPause} className="cursor-pointer" />
     ) : (
       <BsFillPlayFill size={45} color="#FFF" onClick={handlePlayPause} className="cursor-pointer" />
     )}
+    {/* Next song */}
     {currentSongs?.length && <MdSkipNext size={30} color="#FFF" className="cursor-pointer" onClick={handleNextSong} />}
     {/* Shuffle */}
     <BsShuffle size={20} color={shuffle ? '#301934' : 'white'} onClick={() => setShuffle((prev) => !prev)} className="hidden sm:block cursor-pointer" />
